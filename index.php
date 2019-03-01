4b5ef9512192267cc15db9eaed899cdb6d58a34a
Rodrigo Esteves <contact@rodrigoesteves.fr>
2019-03-01 16:38:20 +0100

    Bug Fix:FontAwesome FontFamily

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<meta http-equiv="X-UA-Compatible" content="ie=edge">
 		<title>Admin Dashboard</title>
-		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
 		<link rel="stylesheet" type="text/css" href="./assets/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" type="text/css" href="./css/widget.css">
 		<link rel="stylesheet" type="text/css" href="./css/style.css">
